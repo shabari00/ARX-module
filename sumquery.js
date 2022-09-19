3abb26d3fe914f9048cddc37e1f33f0b37be5055
shabari00 <shabarigirishags.59@gmail.com>
2022-09-19 22:51:07 +0530

    initial commit

---
+++ sumquery.js
@@ -0,0 +1,27 @@
+/*
+Sum query
+You are given an array of N numbers and q queries. Each query contains 2 integers, say L and R. The answer to the query is the sum of the elements between the indices L and R (both inclusive).
+
+Input
+First line contains 2 space separated integers N and q. Second line contains N space separated integers. Next q lines contain 2 space separated integers.
+
+Output
+q lines containing one integer each, denoting the answer corresponding to each query.
+
+Example
+Input:
+
+5 2
+
+20 -5 33 0 87
+
+2 4
+
+0 3
+
+Output:
+
+120
+
+48
+*/
